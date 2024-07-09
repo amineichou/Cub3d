1b617719e72e7f68347c205d64bbb63aec3d0b15
amineichou <ichouamine2020@gmail.com>
2024-07-09 17:22:09 +0100

    add fun prototype to header

--- cub.h
+++ cub.h
@@ -6,7 +6,7 @@
 /*   By: moichou <moichou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 10:52:35 by moichou           #+#    #+#             */
-/*   Updated: 2024/07/09 13:26:47 by moichou          ###   ########.fr       */
+/*   Updated: 2024/07/09 16:54:20 by moichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,48 @@
 
 # include <unistd.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <stdbool.h>
 # include <math.h>
 # include <mlx.h>
 # include <fcntl.h>
+# include <limits.h>
+
+# define BUFFER_SIZE 500
+
+typedef struct s_garbage
+{
+	void				*allocated;
+	struct s_garbage	*next;
+}	t_garbage;
+
+typedef enum s_g_malloc
+{
+	ALLOCATE,
+	FREE,
+}	t_g_malloc;
 
 /* define errors */
-# define OPEN_ERR "can't open map file\n"
+# define OPEN_ERR "can't open the configuration file\n"
 # define NO_EXT_ERR "invalid file, file has no extension (.cub)\n"
 # define EXT_ERR "invalid extension for map filename (.cub)\n"
+
+// garbage collector
+void	*g_malloc(size_t size, t_g_malloc mode);
 
 /* utils */
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
 void	ft_printerror(char *msg);
+char	*ft_strrchr(char *s, int c);
+char	*ft_strjoin(char *first, char *second);
+int		ft_strsearch(char *str, char c);
+char	*ft_strdup(char *str);
+
+/* get_next_line */
+char	*read_all_file(int fd);
 
 /* parser */
-
 int		map_vaidator(char *filename);
 int		parser(int ac, char **filename);
 
